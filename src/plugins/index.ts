cb9cdfe9ff5379173b18e434b5311f46593ff162
Alba Rodriguez <codebioandmore@gmail.com>
2024-03-20 18:41:39 +0100

    Add pinia promotions store

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -5,11 +5,16 @@
  */
 
 // Plugins
+import { pinia } from '../stores';
 import vuetify from './vuetify'
 
 // Types
 import type { App } from 'vue'
 
+//Pinia
+
+
 export function registerPlugins (app: App) {
+  app.use(pinia);
   app.use(vuetify)
 }
